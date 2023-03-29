eba00c2ae7a180774ba882d5aac07a0e63ccdcf7
Cespriet <90179216+Cespriet@users.noreply.github.com>
2023-03-29 02:16:11 +0200

    update

--- trice.js
+++ trice.js
@@ -1,26 +1,37 @@
-function func(i){
-    document.getElementById("result").textContent+=i
-    console.log(i)
-}
+class BaseCalculator {
+    constructor(a) {
+        
+     
+    }
 
-function funcvide(){
-    document.getElementById("result").textContent=""
-    document.getElementById("calcul").textContent=""
+    func(a){
+        document.getElementById("result").textContent+=a
+        console.log(a)
+    }
+    
+    funcvide(){
+        document.getElementById("result").textContent=""
+        document.getElementById("calcul").textContent=""
+    }
+    
+    result(a){
+        console.log(a)
+        let b=eval(a)
+        document.getElementById("calcul").textContent=a
+        document.getElementById("calcul").textContent+="="
+        document.getElementById("result").textContent=b
+        console.log(b)
+    
+    }
+    
+    supp(a){
+        console.log(a)
+        a = a.substring(0, a.length - 1);
+        console.log(a)
+        document.getElementById("result").textContent=a
+    }
+    
+     
 }
+let baseCalculator = new BaseCalculator();
 
-function result(a){
-    console.log(a)
-    b=eval(a)
-    document.getElementById("calcul").textContent=a
-    document.getElementById("calcul").textContent+="="
-    document.getElementById("result").textContent=b
-    console.log(b)
-
-}
-
-function supp(a){
-    console.log(a)
-    a = a.substring(0, a.length - 1);
-    console.log(a)
-    document.getElementById("result").textContent=a
-}
